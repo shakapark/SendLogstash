87da14723f4ece685fa2d081b4200e2a8ac4e059
Shakapark <shakapark1994@gmail.com>
2018-04-06 15:16:30 +0200

    Revert "Change Logstash Lib"
    This reverts commit ac8b986e89d4e0aab97ed5bb2dc18f05bac1f9f2.

--- main.go
+++ main.go
@@ -3,9 +3,8 @@ package main
 import (
 	"fmt"
 	"os"
-	"strconv"
 
-	"github.com/mickep76/go-logstash"
+	"github.com/heatxsink/go-logstash"
 	"gopkg.in/alecthomas/kingpin.v2"
 
 	"github.com/shakapark/SendLogstash/config"
@@ -24,12 +23,17 @@ func main(){
 	}
 
 	for _, server := range sc.C.Servers {
-		l := logstash.New(server.Host+":"+strconv.Itoa(server.Port), 5)
-		go l.Start()
-		defer l.Stop()
+		l := logstash.New(server.Host, server.Port, 5)
+		_, err := l.Connect()
+		if err != nil {
+			fmt.Println(err)
+		}
 
 		for _, entry := range server.Entries {
-			l.Info(entry)
+			err = l.Writeln(entry)
+			if err != nil {
+				fmt.Println(err)
+			}
 		}
 	}
 }
